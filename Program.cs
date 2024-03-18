611b6f937f018c473637208b4db160c52ae476f8
Lakshmi <lakshmiynh8@gmail.com>
2024-03-18 19:15:13 +0530

    added:main method class file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ContactDetails;
+
+Console.WriteLine("Hello, World!");
+
+Adddetails customerdetails1 = new Adddetails();
+customerdetails1.Addingdetails();
+Console.WriteLine("check your details");
+customerdetails1.display();
+
+customerdetails1.edit();
+customerdetails1.display();
+customerdetails1.detele();
